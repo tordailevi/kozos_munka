39c920575db5a53779e6d27e063ba899ae2b2fa9
AWAVAUATVWUSH <agent.maine.the.meta12@gmail.com>
2024-10-25 13:33:59 +0200

    Now actually done

--- getorder.py
+++ getorder.py
@@ -1,16 +1,15 @@
 def get_order(orderedItems:list):
-    a:int=1
-    while a !=0:
-        c:int=0
+    itemNumber:int=1
+    while itemNumber !=0:
+        i:int=0
         try:
-            a:int=int(input("Kérem adja meg a választott étel sorszámát! (ha már nem szeretne rendelni, írjon be 0-át): "))
-            if ((a == 0) or (6 < a) or (0 > a)):
+            itemNumber:int=int(input("Kérem adja meg a választott étel sorszámát! (ha már nem szeretne rendelni, írjon be 0-át): "))
+            if ((itemNumber == 0) or (6 < itemNumber) or (0 > itemNumber)):
                 continue
             else:
-                b:int=int(input("Kérem adja meg, hogy a megadott ételből hány tételt szeretne rendelni!: "))
-            while b-1 >= c:
-                orderedItems.append(a-1)
-                c+=1
+                itemAmount:int=int(input("Kérem adja meg, hogy a megadott ételből hány tételt szeretne rendelni!: "))
+            while itemAmount-1 >= i:
+                orderedItems.append(itemNumber-1)
+                i+=1
         except ValueError:
             print("Hiba, kérem számadatot adjon meg adjon meg!")
-    return(orderedItems)
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from getorder import get_order
 from receipt_print import receipt_print
 
 menuItems:list=["1) Palacsinta", "2) Pörkölt", "3) Rántotthús", "4) Húsleves", "5) Bableves", "6) Frankfurti leves"]
-orderedItems:list=[]
 menuItemPrices:list=[800, 2500, 3000, 1500, 1300, 1900]
+orderedItems:list=[]
 
 menu_print(30,menuItems,menuItemPrices)
 
