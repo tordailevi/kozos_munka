5cb3a874aee06b6341ab6dd404c520c7a9544c72
tordailevi <tordailevigmail.com>
2024-10-25 12:13:44 +0200

    étlap

--- fprint_lib.py
+++ fprint_lib.py
@@ -16,9 +16,26 @@ def simbolprint(linelength:int=10, symbol:str=" ", border:bool=False):
 # price of the item
 # should there be a border?
 
-def itemprint(linelength:int=10, symbol:str=" ", itemName:str="", itemPrice:int=0, border:bool=False):
+def itemprint(linelength:int=10, symbol:str=" ", itemName:str="", itemPrice:int=0, border:bool=True):
     itemPriceText = f"{itemPrice}"
     if(border == False):
         print(f"{itemName}{(symbol)*(linelength-(len(itemName)+len(itemPriceText)+2))}{itemPrice}Ft")
     else:
         print(f"|{itemName}{(symbol)*(linelength-(len(itemName)+len(itemPriceText)+4))}{itemPrice}Ft|")
+
+
+def jelek(jel:str, hanyszor:int=1):
+    print(jel*hanyszor)
+
+def nyugta_kiiras(szo:str = "",hossz:int=24, jel:str="*",):
+    hossz=hossz-2
+    szo=szo.upper()
+    print(f"{jel}{szo:^{hossz}}{jel}" )
+def lec(szo:str="",hossz:int=24,jel:str="*"):
+    jelek(jel,hossz)
+    nyugta_kiiras(szo,hossz,jel)
+    jelek(jel,hossz)
+
+def tetelkiiras(tetel:str="", t1:float=0, hossz:int=24):
+    hossz=hossz-len(tetel)
+    print(f"{tetel}{t1:>{hossz}}")
--- main.py
+++ main.py
@@ -3,23 +3,19 @@ import fprint_lib
 menuItems:list=["Palacsinta", "Pörkölt", "Rántotthús", "Húsleves", "Bableves", "Frankfurti leves"]
 orderedItems:list=[]
 menuItemPrices:list=[800, 2500, 3000, 1500, 1300, 1900]
-#printMenu(menuItems)
 
-#getorder(orderedItems, menuItemPrices)
-
-
-
-fprint_lib.simbolprint(30, "*")
-fprint_lib.itemprint(30, "_", f"{menuItems[0]}", 800)
-fprint_lib.simbolprint(30, "=")
-fprint_lib.itemprint(30, "_", f"{menuItems[1]}", 2500)
-fprint_lib.simbolprint(30, "=")
-fprint_lib.itemprint(30, "_", f"{menuItems[2]}", 3000)
-fprint_lib.simbolprint(30, "=")
-fprint_lib.itemprint(26, "_", f"{menuItems[3]}", 1500)
-fprint_lib.simbolprint(30, "=")
-fprint_lib.itemprint(26, "_", f"{menuItems[4]}", 1300)
-fprint_lib.simbolprint(30, "=")
-fprint_lib.itemprint(26, "_", f"{menuItems[5]}", 1900)
-fprint_lib.simbolprint(30, "*")
+fprint_lib.lec("Étlap", 30, "*")
+fprint_lib.itemprint(30, ".", f"{menuItems[0]}", 800, "*")
+fprint_lib.simbolprint(30, "=", "*")
+fprint_lib.itemprint(30, ".", f"{menuItems[1]}", 2500, "*")
+fprint_lib.simbolprint(30, "=", "*")
+fprint_lib.itemprint(30, ".", f"{menuItems[2]}", 3000, "*")
+fprint_lib.simbolprint(30, "=", "*")
+fprint_lib.itemprint(30, ".", f"{menuItems[3]}", 1500, "*")
+fprint_lib.simbolprint(30, "=", "*")
+fprint_lib.itemprint(30, ".", f"{menuItems[4]}", 1300, "*")
+fprint_lib.simbolprint(30, "=", "*")
+fprint_lib.itemprint(30, ".", f"{menuItems[5]}", 1900, "*")
+fprint_lib.simbolprint(30, "-", "*")
+fprint_lib.simbolprint(30, "*", "*")
 
